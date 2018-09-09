bb70d7c8cc11e15049b30e0ce11a2bb24562a786
Hmamouche <youssef.hmamouche@lif.univ-mrs.fr>
2018-09-09 15:58:07 +0200

    add plots scripts

---
+++ plot/run.sh
@@ -0,0 +1,7 @@
+python matrix-eval.py ../data/*aus*
+python matrix-eval.py ../data/*us_diff*
+#python matrix-eval.py ../data/*Transaction*
+
+python models-eval.py ../data/*aus*
+python models-eval.py ../data/*us_diff*
+#python models-eval.py ../data/*Transaction*

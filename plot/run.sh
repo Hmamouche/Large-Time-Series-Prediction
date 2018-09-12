5c0c79dcb76e3f98ee77571bef3ee0d086ebbfc1
Hmamouche <youssef.hmamouche@lif.univ-mrs.fr>
2018-09-12 02:16:10 +0200

    change foalder name

--- plot/run.sh
+++
@@ -1,7 +0,0 @@
-python matrix-eval.py ../data/*aus*
-python matrix-eval.py ../data/*us_diff*
-#python matrix-eval.py ../data/*Transaction*
-
-python models-eval.py ../data/*aus*
-python models-eval.py ../data/*us_diff*
-#python models-eval.py ../data/*Transaction*

c6cf1b685c904c05f1a8e83178c6700a5cc43927
Hmamouche <youssef.hmamouche@lif.univ-mrs.fr>
2018-09-12 17:36:41 +0200

    to generate the figures

---
+++ generate_plots.sh
@@ -0,0 +1,7 @@
+python plots/matrix-eval.py data/*aus*
+python plots/matrix-eval.py data/*us_diff*
+python plots/matrix-eval.py data/*Transaction*
+
+python plots/models-eval.py data/*aus*
+python plots/models-eval.py data/*us_diff*
+python plots/models-eval.py data/*Transaction*

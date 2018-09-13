b44adbece94c5784f4790dd2c97bc785679c8631
Hmamouche <youssef.hmamouche@lif.univ-mrs.fr>
2018-09-13 09:49:05 +0200

    update requirements

--- requirements.R
+++ requirements.R
@@ -1,15 +1,21 @@
-chooseCRANmirror()
-install.packages(c("kernlab", 
-					"TransferEntropy")
-					"forecast",
-					"cluster",
-					"lmtest",
-					"fpc",
-					"vars",
-					"Biocomb",
-					"stats",
-					"parallel",
-					"tsDyn",
-					"factoextra",
-					"NbClust",
-					"psych"))
+# Some of these packages should not be installed, but can be installed manually, like rJava and TransferEntropy
+# for rJava, see: https://zhiyzuo.github.io/installation-rJava/
+
+
+packages = c ("kernlab", 
+			"TransferEntropy",
+			"forecast",
+			"cluster",
+			"lmtest",
+			"fpc",
+			"vars",
+			"Biocomb",
+			"rJava",
+			"stats",
+			"parallel",
+			"tsDyn",
+			"factoextra",
+			"NbClust",
+			"psych")
+install.packages (packages, repos='http://cran.us.r-project.org')
+

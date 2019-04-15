c1c23948d7a7a7f57d843062e3891ad9e266a5bd
Hmamouche <youssef.hmamouche@lif.univ-mrs.fr>
2019-04-15 10:01:36 +0200

    Update test.sh

--- spark/test.sh
+++ spark/test.sh
@@ -1,6 +1,7 @@
 # This a sample test of the process  on australian macro-economic dataset.
 
-# Put data to hdfs
+# To make an example, we create an RDD data from local csv file and put it to hdfs
+# This not necessarely if you data is already in hdfs
 sh jobs/local_to_distributed.sh data/ausmacro.csv
 
 # Compute the Granger causality graph
@@ -9,5 +10,5 @@ sh jobs/causality_mat.sh data/ausmacro
 # Execute the Pehar algorithm (feature selection)
 sh jobs/pehar_dist.py ausmacro_gc
 
-# Compute prediction
+# Compute predictions
 sh jobs/prediction.sh ausmacro
